db9f047247f1b505ad3b38afbf2506b2d72df071
Craftov <115530415+Craftov@users.noreply.github.com>
2025-04-07 20:44:40 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+
+window.onload = function () {
+  setTimeout(() => {
+    document.querySelector('.loader-screen').style.display = 'none';
+    document.querySelector('#terminal').style.display = 'block';
+  }, 2000);
+};
